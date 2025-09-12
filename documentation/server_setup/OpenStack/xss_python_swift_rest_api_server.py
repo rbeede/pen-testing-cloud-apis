3d43ae4e6daf3878268cdb27aeb222e11f9ee4d0
Rodney Beede <rbeede@users.noreply.github.com>
2025-09-12 12:10:55 -0500

    Enhanced sample XSS page

--- documentation/server_setup/OpenStack/xss_python_swift_rest_api_server.py
+++ documentation/server_setup/OpenStack/xss_python_swift_rest_api_server.py
@@ -1,12 +1,19 @@
 # Uses Python3 http.server for serving requests
-# Rodney Beede - 2023-03
+# Rodney Beede - 2025-09
+
+#!!!!! IMPORTANT!!!!
+# If using Python 3.13 or later you will need to install legacy-cgi
+# https://pypi.org/project/legacy-cgi/
+import cgi
+#!!!!!!!!!
 
 import http.server
 import logging
+import string
 import sys
 import urllib3
 
-from swiftclient.service import SwiftService, SwiftError
+from swiftclient.service import SwiftService, SwiftError, SwiftUploadObject
 
 
 def main(argv):
@@ -61,12 +68,15 @@ def HandlerFactory(container, swift_opts):
                 return
 
         
-            response_content = "<html><body>\r\n"
-            response_content += "<input type='button' value='Upload' onclick=\"alert('This simulation would upload a file but only allow characters a-z and nothing else in the filename. No XSS for you.')\"/>\r\n"
-            response_content += "<h1>\r\n"
-            response_content += "List of Uploaded Files\r\n"
-            response_content += "</h1>\r\n"
-            response_content += "<pre>\r\n"
+            response_content = "<html><body>"
+            response_content += "<form action='/REST/API/endpoint.cgi' method='post' enctype='multipart/form-data'>"
+            response_content += "Upload a file: <input type='file' name='file'/><input type='submit' />"
+            response_content += "</form>"
+            response_content += "<hr/>"
+            response_content += "<h1>"
+            response_content += "List of Uploaded Files"
+            response_content += "</h1>"
+            response_content += "<pre>"
             
             with SwiftService(options = self.swift_opts) as swift:
                 try:
@@ -86,14 +96,14 @@ def HandlerFactory(container, swift_opts):
                 except SwiftError as e:
                     print(e.value)
             
-            response_content += "</pre>\r\n"
-            response_content += "</body></html>\r\n"
+            response_content += "</pre>"
+            response_content += "</body></html>"
 
 
             encoded_response_content = response_content.encode("utf-8")
 
             self.send_response(200)
-            self.send_header("BSidesSATX", "2023")
+            self.send_header("Author", "rodneybeede.com")
             self.send_header("Content-Type", "text/html")
             self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
             self.send_header("Pragma", "no-cache")
@@ -102,7 +112,63 @@ def HandlerFactory(container, swift_opts):
             self.end_headers()
 
             self.wfile.write(encoded_response_content)
+
+
+        def do_POST(self):
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={
+                    'REQUEST_METHOD': 'POST',
+                    'CONTENT_TYPE': self.headers['Content-Type'],
+                })
+
+            filename = form['file'].filename
+            allowlist_chars = set((
+                'a','b','c','d','e','f',
+                'g','h','i','j','k','l',
+                'm','n','o','p','q','r',
+                's','t','u','v','w','x',
+                'y','z','0', '1', '2',
+                '3', '4', '5', '6', '7',
+                '8', '9','.'
+                ))
+            if any(char not in allowlist_chars for char in filename):
+                errmsg_encoded = f"You can only use filenames with characters {','.join(sorted(allowlist_chars))}".encode("utf-8")
+                self.send_response(400)
+                self.send_header('Content-Length', len(errmsg_encoded))
+                self.send_header('X-Hint', 'Think outside of the web server box. Take a nice REST.')
+                self.end_headers()
+                self.wfile.write(errmsg_encoded)
+                return
+
+            #data = form['file'].file.read()
+            
+            with SwiftService(options = self.swift_opts) as swift:
+                try:
+                    for result in swift.upload(
+                        container=self.container,
+                        objects=[
+                            #SwiftUploadObject(source=form['file'].file, object_name=filename),
+                            SwiftUploadObject('/etc/passwd', object_name=filename),
+                        ]):
+                            if not 'success' in result:
+                                print(result['error'])
+                except SwiftError as e:
+                    print(e.value)
+
+
+                msg_encoded = f"File {filename} uploaded successfully".encode("utf-8")
+                self.send_response(200)
+                self.send_header('Content-Length', len(msg_encoded))
+                self.end_headers()
+                self.wfile.write(msg_encoded)
+
+
+    # HandlerFactory
     return VulnerableObjectStorageHTTPRequestHandler  # factory return
+
+
 
 
 if __name__ == "__main__":
